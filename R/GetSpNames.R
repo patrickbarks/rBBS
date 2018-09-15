9587c7b9f99ed0fbc14bdce0f278174ea8e84bbd
patrickbarks <patrick.barks@gmail.com>
2018-09-15 15:59:01 +0200

    changed GetSpNames to use utils::read.fwd and deleted commented out sections

--- R/GetSpNames.R
+++ R/GetSpNames.R
@@ -26,41 +26,26 @@
 #' @examples
 #' Species <- GetSpNames()
 #' 
+#' @importFrom utils read.fwf
 #' @export GetSpNames
 GetSpNames <- function(Dir="ftp://ftpext.usgs.gov/pub/er/md/laurel/BBS/DataFiles/") {
+
+  
   File <- paste0(Dir, "SpeciesList.txt")
   
   All <- scan(File, what="character", sep="\n", encoding="latin1")
+  
   Delimiter <- grep("^-", All)
   
   ColNames <- strsplit(All[Delimiter-1], split='[[:blank:]]+')[[1]]
-  Widths <- nchar(strsplit(All[Delimiter], split='[[:blank:]]+')[[1]])
+  Widths <- nchar(strsplit(All[Delimiter], split='[[:blank:]]+')[[1]]) + 1
   
-  Lines <- sapply(All[-(1:Delimiter)], function(str, w) {
-    trimws(substring(str, c(1,cumsum(w[-length(w)])), cumsum(w)))
-  }, w=Widths+1)
-  colnames(Lines) <- NULL
-  rownames(Lines) <- ColNames
+  df <- read.fwf(File, skip = Delimiter, widths = Widths,
+                 fileEncoding = 'Latin1', strip.white = TRUE,
+                 stringsAsFactors = FALSE, col.names = ColNames)
   
-  Lines.df <- as.data.frame(t(Lines), stringsAsFactors = FALSE)
-  Lines.df$Seq <- as.numeric(Lines.df$Seq)
-  Lines.df$AOU <- as.numeric(Lines.df$AOU)
-  Lines.df
+  df$AOU <- formatC(df$AOU, width = 5, flag = '0')
+  
+  return(df)
 }
 
-
-# GetSpNames <- function(Dir="ftp://ftpext.usgs.gov/pub/er/md/laurel/BBS/DataFiles/") {
-#   File <- paste0(Dir,"SpeciesList.txt")
-#   spnames.all <- scan(File, sep="\n", what=character(), blank.lines.skip = FALSE, fileEncoding="Latin1")
-#   SpCols <- spnames.all[-(1:grep("^-", spnames.all))]
-#   
-#   Widths <- c(1,1+cumsum(nchar(strsplit(spnames.all[8], ' ')[[1]])))
-#   
-#   SpData <- as.data.frame(sapply(2:length(Widths), function(wh, wid, dat) {
-#     trimws(substr(dat, wid[wh-1]+1, wid[wh]))
-#   }, wid=Widths, dat=SpCols), stringsAsFactors = FALSE)
-#   names(SpData) <- strsplit(spnames.all[7], '[ ]{2,}')[[1]]
-#   SpData$Seq <- as.numeric(SpData$Seq)
-#   SpData$AOU <- as.numeric(SpData$AOU)
-#   SpData
-# }
