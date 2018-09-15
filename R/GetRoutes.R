c0ea0fad96160108fb87cd960f6f48fed4a076b1
patrickbarks <patrick.barks@gmail.com>
2018-09-15 12:58:43 +0200

    changed 'Routes.zip' to 'routes.zip' to reflect the 2017 version

--- R/GetRoutes.R
+++ R/GetRoutes.R
@@ -37,7 +37,7 @@
 #' 
 #' @export GetRoutes
 GetRoutes <- function(Dir="ftp://ftpext.usgs.gov/pub/er/md/laurel/BBS/DataFiles/") {
-  routes=GetUnzip(ZipName=paste0(Dir, "Routes.zip"), FileName="routes.csv")
+  routes=GetUnzip(ZipName=paste0(Dir, "routes.zip"), FileName="routes.csv")
   routes$routeID=paste(routes$statenum, routes$Route)
   routes
 }
