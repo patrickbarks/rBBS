786cb8f4e8424b9b39e5e12c0978e14e55bfefca
patrickbarks <patrick.barks@gmail.com>
2018-09-20 12:28:47 +0200

    moved csv_unzip from own file to zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -16,6 +16,45 @@ bbs_read_dir <- function(dir) {
 }
 
 
+
+#' @importFrom utils download.file
+#' @importFrom readr read_csv
+#' @export csv_unzip
+csv_unzip <- function(zip_path) {
+  
+  zip_path_split <- strsplit(zip_path, '/+')[[1]]
+  file_zip <- zip_path_split[length(zip_path_split)]
+  file_csv <- gsub('\\.zip', '\\.csv', file_zip)
+  temp_dir <- paste0(tempdir(), '/', paste(sample(letters, 6), collapse = ''))
+  dir.create(temp_dir)
+  
+  if(grepl('^http:|^ftp:', zip_path)) {
+    temp_file <- tempfile()
+    download.file(zip_path, temp_file)
+    unzip(temp_file, exdir = temp_dir)
+    unlink(temp_file)
+  } else {
+    unzip(zip_path, exdir = temp_dir)
+  }
+  
+  file_csv <- list.files(temp_dir)
+  file_csv <- file_csv[grep('\\.csv$', file_csv)]
+  
+  if (length(file_csv) > 1) {
+    stop('Zip archive appears to contain more than one csv file')
+  }
+  
+  dat <- suppressMessages(read_csv(paste0(temp_dir, '/', file_csv),
+                                   na = c('NA', 'NULL'),
+                                   progress = FALSE))
+  
+  names(dat) <- bbs_col(names(dat))
+  
+  unlink(temp_dir)
+  return(dat)
+}
+
+
 #' @importFrom utils read.fwf
 #' @importFrom utils download.file
 #' @importFrom tibble as_tibble
