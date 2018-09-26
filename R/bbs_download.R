d29936e8f46adb6561fad8210cee495f124af011
patrickbarks <patrick.barks@gmail.com>
2018-09-26 16:16:57 +0200

    update doc and remove migrant download option

--- R/bbs_download.R
+++ R/bbs_download.R
@@ -6,17 +6,14 @@
 #' @param bbs_dir Directory from which to get data. Defaults to the USGS FTP
 #'   directory for the most recent BBS release. May alternatively be an FTP
 #'   address for an older BBS release.
-#' @param countries Vector of countries to download 10-stop data for (e.g.
-#'   \code{c('Canada', 'United States')}). Used only if \code{ten_stop = TRUE}.
-#'   Defaults to all available. Case-insensitive. See Details.
-#' @param states Vector of states/provinces/territories to download 10-stop data
-#'   for. Used only if \code{ten_stop = TRUE}. Defaults to all available.
+#' @param countries Vector of countries to download 10- and/or 50-stop data for
+#'   (e.g. \code{c('Canada', 'United States')}). Defaults to all available.
 #'   Case-insensitive. See Details.
+#' @param states Vector of states/provinces/territories to download 10- and/or
+#'   50-stop data for. Defaults to all available. Case-insensitive. See Details.
 #' @param meta Download top-level metadata files? Default is \code{TRUE}.
 #' @param ten_stop Download 10-stop data? Default is \code{TRUE}.
 #' @param fifty_stop Download 50-stop data? Default is \code{FALSE}.
-#' @param migrant Download data for migrant non-breeders? Default is
-#'   \code{FALSE}.
 #' @param overwrite Overwrite files that already exist? Default is \code{FALSE}.
 #' @param verbose List files that were not re-downloaded because they already
 #'   exist? Default is \code{TRUE}. Only applies if \code{overwrite = FALSE}.
@@ -28,14 +25,14 @@
 #' Note that country/state subsets are additive, so specifying
 #' 
 #' \code{
-#' bbs_download('.', countries = 'Canada', states = 'Montana')
+#' bbs_download(".", countries = "Canada", states = "Montana")
 #' }
 #' 
 #' will download data for all Canadian provinces/territories plus the state of
 #' Montana. The following lines will both download data for all American states:
 #' 
-#' \code{bbs_download('.', countries = 'United States', states = 'Florida')} \cr
-#' \code{bbs_download('.', countries = 'United States')}
+#' \code{bbs_download(".", countries = "United States", states = "Florida")} \cr
+#' \code{bbs_download(".", countries = "United States")}
 #' 
 #' @author Bob O'Hara
 #' @author Patrick Barks <patrick.barks@@gmail.com>
@@ -48,25 +45,21 @@
 #' \dontrun{
 #' 
 #' # download metadata and 10-stop data for Alaska and all of Canada
-#' bbs_download(dest = '.', countries = 'Canada', states = c('Alaska'))
+#' bbs_download(dest = ".", countries = "Canada", states = "Alaska")
 #' 
 #' # download metadata and 10-stop data for Gulf Coast states
-#' gulf_states <- c('Alabama', 'Florida', 'Louisiana', 'Mississippi', 'Texas')
-#' bbs_download(dest = '.', states = gulf_states)
+#' gulf_states <- c("Alabama", "Florida", "Louisiana", "Mississippi", "Texas")
+#' bbs_download(dest = ".", states = gulf_states)
 #' 
 #' # download metadata and 50-stop data for all regions
-#' bbs_download(dest = '.', ten_stop = FALSE, fifty_stop = TRUE)
+#' bbs_download(dest = ".", ten_stop = FALSE, fifty_stop = TRUE)
 #' }
 #' 
 #' @export bbs_download
 bbs_download <- function(dest, bbs_dir = NULL, countries = NULL, states = NULL,
                          meta = TRUE, ten_stop = TRUE, fifty_stop = FALSE,
-                         migrant = FALSE, overwrite = FALSE, verbose = TRUE) {
+                         overwrite = FALSE, verbose = TRUE) {
   
-  # check whether R build has libcurl capability
-  if (capabilities("libcurl") == FALSE) {
-    stop('This function requires libcurl capability')
-  }
   # check whether dest exists
   if (!dir.exists(dest)) {
     stop("Destination directory not found. Argument 'dest' must be a path
