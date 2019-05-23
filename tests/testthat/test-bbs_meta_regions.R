d8196fe4542c28fe62eb4387aaceee9b7ed93810
patrickbarks <patrick.barks@gmail.com>
2019-05-23 10:03:54 +0200

    add test for zip files

--- tests/testthat/test-bbs_meta_regions.R
+++ tests/testthat/test-bbs_meta_regions.R
@@ -2,19 +2,21 @@ context("bbs_meta_regions")
 
 test_that("bbs_meta_regions works correctly", {
   bbs_dir <- paste0(system.file("testdata", package = "rBBS"), '/')
-  regions <- bbs_meta_regions(bbs_dir = bbs_dir, zip_files = TRUE)
   
-  zip10 <- regions$ten_stop_file[!is.na(regions$ten_stop_file)]
-  zip50 <- regions$fifty_stop_file[!is.na(regions$fifty_stop_file)]
+  regions <- bbs_meta_regions(bbs_dir = bbs_dir)
+  regions_zip <- bbs_meta_regions(bbs_dir = bbs_dir, zip_files = TRUE)
+  
+  zip10 <- regions_zip$ten_stop_file[!is.na(regions_zip$ten_stop_file)]
+  zip50 <- regions_zip$fifty_stop_file[!is.na(regions_zip$fifty_stop_file)]
 
-  expect_is(regions, "data.frame")
-  expect_is(regions, "tbl_df")
+  expect_true(ncol(regions_zip) == ncol(regions) + 2)
+  expect_true(all(c("data.frame", "tbl_df") %in% class(regions)))
   expect_is(regions$country_num, "integer")
   expect_is(regions$country_name, "character")
   expect_is(regions$state_num, "integer")
   expect_is(regions$state_name, "character")
-  expect_is(regions$ten_stop_file, "character")
-  expect_is(regions$fifty_stop_file, "character")
+  expect_is(regions_zip$ten_stop_file, "character")
+  expect_is(regions_zip$fifty_stop_file, "character")
   expect_true(all(grepl("\\.zip", zip10)))
   expect_true(all(grepl("\\.zip", zip50)))
 })
